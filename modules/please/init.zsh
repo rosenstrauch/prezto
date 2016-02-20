7f065208234ecac4b7f2799ba322b0480ea4bec9
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2016-02-21 00:49:38 +0100

    keyboard shortcuts

--- modules/please/init.zsh
+++ modules/please/init.zsh
@@ -2,4 +2,4 @@ alias please='sudo $(fc -ln -1)'
 
 previous_command_with_sudo() { zle beginning-of-line; zle -U "sudo $(fc -ln -1)" }
 zle -N previous-command-with-sudo previous_command_with_sudo
-bindkey "\es" previous-command-with-sudo # alt + S
+bindkey "^[#" previous-command-with-sudo # alt + #
